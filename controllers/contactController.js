d7b46d34c0ed1a0ad0d801b71812ecbe0f7761f9
Saman Hazemi <s.hazemi.jebelli@gmail.com>
2023-12-20 17:42:37 +0330

    feat: crud capability for mongo cloud data base

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -36,8 +36,12 @@ const createContact = asyncHandler(async (req, res) => {
 //@access public
 
 const getContact = asyncHandler(async (req, res) => {
-    res.status(200).json({"msg": "Get contact for " +
-            `${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
 });
 
 //@desc Update a contact
@@ -45,17 +49,31 @@ const getContact = asyncHandler(async (req, res) => {
 //@access public
 
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({"msg": "Update contact for " +
-            `${req.params.id}`})
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    );
+    res.status(200).json(updatedContact);
 });
 
 //@desc Delete a contact
 //@route DELETE /api/contacts/:id
 //@access public
 
-const deleteContact = asyncHandler(async (req, res) => (req, res) => {
-    res.status(200).json({"msg": "Delete contact for " +
-            `${req.params.id}`});
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    await Contact.findByIdAndDelete(req.params.id);
+    res.status(200).json({message: `Delete contact for ${req.params.id}`});
 });
 
 module.exports = {

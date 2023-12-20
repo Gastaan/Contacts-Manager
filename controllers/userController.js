fc5b63985dacdf4bf8533c4310f23c6d8f180a3b
Saman Hazemi <s.hazemi.jebelli@gmail.com>
2023-12-20 19:26:26 +0330

    feat: user registiration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,45 @@
 const asyncHandler = require("express-async-handler")
 const User = require("../models/userModel")
+const bcrypt = require("bcrypt")
+const {hash} = require("bcrypt");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({message: "Register the user"})
+    const {username, email, password} = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const userAvailable =  await User.findOne({email});
+
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Hashed Password
+    console.log(hashedPassword);
+
+    const user = await User.create(
+        {
+            username,
+            email,
+            password: hashedPassword
+        }
+    )
+
+    console.log(`User created ${user}`)
+    if (user) {
+        res.status(201).json({_id: user.id, email: user.email});
+    }
+    else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
 });
 
 //@desc login a user

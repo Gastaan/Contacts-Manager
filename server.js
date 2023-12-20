287eaf1e5cffdb81ed1a1356c6311e62e45b8e88
Saman Hazemi <s.hazemi.jebelli@gmail.com>
2023-12-20 13:58:38 +0330

    feat: error handler & constants

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler")
 const port = process.env.PORT || 3031
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)

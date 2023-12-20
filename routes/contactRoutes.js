59ae5aaaf3020a6649ca7c20136034f8bb90500e
Saman Hazemi <s.hazemi.jebelli@gmail.com>
2023-12-20 12:54:56 +0330

    feat: initiating a router

---
+++ routes/contactRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.route("/").get((req, res) => {
+    res.status(200).json({"msg": "get all contacts"});
+});
+
+router.route("/").post((req, res) => {
+    res.status(200).json({"msg": "Create contact"});
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(200).json({"msg": "Update contact for " +
+            `${req.params.id}`});
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).json({"msg": "Get contact for " +
+            `${req.params.id}`});
+});
+
+router.route("/:id").delete((req, res) => {
+    res.status(200).json({"msg": "Delete contact for " +
+            `${req.params.id}`});
+});
+
+module.exports = router;

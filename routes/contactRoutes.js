2ede0b1df27503d3ab62d1f5727172e757169e2e
Saman Hazemi <s.hazemi.jebelli@gmail.com>
2023-12-20 13:28:10 +0330

    style: cleaner requirement in the contact router

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {getContacts} = require("../controllers/contactController")
-const {createContact} = require("../controllers/contactController")
-const {getContact} = require("../controllers/contactController")
-const {updateContact} = require("../controllers/contactController")
-const {deleteContact} = require("../controllers/contactController")
+const {
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact
+} = require("../controllers/contactController")
 
 router.route("/").get(getContacts);
 
